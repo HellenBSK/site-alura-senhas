67dcbbaea07911500aef107f32ecb555680b819b
HellenBSK <144059781+HellenBSK@users.noreply.github.com>
2025-05-25 20:29:36 -0300

    ok

--- main.js
+++ main.js
@@ -9,16 +9,14 @@ botoes[1].onclick = aumentaTamanho;
 
 function diminuiTamanho(){
     if (tamanhoSenha > 1){
-        //tamanhoSenha = tamanhoSenha-1;
-        tamanhoSenha--;
+        tamanhoSenha = tamanhoSenha-1;
     }
     
     numeroSenha.textContent = tamanhoSenha;
 }
 function aumentaTamanho(){
     if (tamanhoSenha < 20){
-       //tamanhoSenha = tamanhoSenha+1;
-       tamanhoSenha++;
+       tamanhoSenha = tamanhoSenha+1;
     }
     
     numeroSenha.textContent = tamanhoSenha;
